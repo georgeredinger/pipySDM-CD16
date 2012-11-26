5b5010bed123f4d60ee578177e8ec931d6e228ad
George Redinger <george@georgeredinger.com>
2012-11-25 17:58:50 -0800

    wiggling the correct pins

--- test.py
+++ test.py
@@ -4,20 +4,21 @@ import time
 HIGH = GPIO.HIGH
 LOW  = GPIO.LOW
 
-C1 = 22
-C2 = 23
-C3 = 24
-SDMCD16ADDRESS =  int('101010101',2)
-FILL = int('1000000000000000',2)
+C1 = 22 #data
+C2 = 23 #clock
+C3 = 24 #enable
 
+SDMCD16ADDRESS =  int('11111111',2)
+FILL = int('0000000000000000',2)
+
+#def setup_GPIO():
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(C1, GPIO.OUT)
 GPIO.setup(C2, GPIO.OUT)
 GPIO.setup(C3, GPIO.OUT)
-GPIO.output(C1, LOW)
-GPIO.output(C2, LOW)
-GPIO.output(C3, LOW)
-
+GPIO.output(C1, GPIO.LOW)
+GPIO.output(C2, GPIO.LOW)
+GPIO.output(C3, GPIO.LOW)
 
 def clock_a_bit(bit):
   if bit == 1:
@@ -41,20 +42,25 @@ def clock_a_word(word):
     w = w >> 1
 
 def latch_SDM():
-  GPIO.output(C3,LOW)
-  GPIO.output(C3,HIGH)
+  GPIO.output(C3,GPIO.LOW)
+  GPIO.output(C3,GPIO.HIGH)
 
 def enable_SDM():
-  GPIO.output(C2,HIGH)
-  GPIO.output(C3,HIGH)
-#
-enable_SDM
-#clock_a_byte(SDMCD16ADDRESS)
-#clock_a_word(FILL)
-latch_SDM
+  GPIO.output(C2,GPIO.HIGH)
+  GPIO.output(C3,GPIO.HIGH)
+
+def disable_SDM():
+  GPIO.output(C2,GPIO.LOW)
+  GPIO.output(C3,GPIO.LOW)
 
 
-
+while True:
+  enable_SDM()
+  clock_a_byte(SDMCD16ADDRESS)
+  clock_a_word(FILL)
+  latch_SDM()
+  disable_SDM()
+  time.sleep(1)
 
 
 
